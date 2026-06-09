533cc608369aee1fe87b5dd4eb1bee24fc7231e3
lubkoserhii <lubko.serhii@gmail.com.com>
2026-06-09 18:58:33 +0300

    Add ProfileViewSet for user profiles

--- user/views.py
+++ user/views.py
@@ -2,9 +2,28 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from user.serializers import UserRegisterSerializer
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
+from user.models import Profile
+from user.serializers import ProfileSerializer
+from user.permissions import IsOwnerOrReadOnly
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
     queryset = get_user_model().objects.all()
     permission_classes = (AllowAny,)
+
+
+class ProfileViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = ["user__username", "bio"]
